c87e0bfedb1e549d048aa625a327da72dc53491a
GreyTheGangalf <erkin.arikan@ozu.edu.tr>
2026-06-23 00:04:05 +0300

    feat: add admin privilege check and multithreaded log reading
    - Implement OS-level admin/root validation (ctypes/os)
    - Add background thread for non-blocking, real-time log reading
    - Redirect stderr to stdout for unified log capturing
    - Ensure memory-safe thread termination with daemon=True

--- main.py
+++ main.py
@@ -1,44 +1,73 @@
 import subprocess
 import sys
 import time
+import threading
+import ctypes
+import os
 
-def __init__(self, executable_path):
-    self.executable_path = executable_path
-    self.process = None
+class DPIBypassManager:
+    def __init__(self, executable_path):
+        self.executable_path = executable_path
+        self.process = None
+        self.is_running = False
 
-def start(self, arguments):
-    print(f"[*] DPI Bypass tool is starting: {self.executable_path}")
-
-    try:
-        command = [self.executable_path] + arguments
-        self.process = subprocess.Popen(
-            command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-        )
-        print("[+] Service has been started successfully. Traffic is now hidden.")
-
-    except FileNotFoundError:
-        print("[-] Error: No  file found to execute. Check the path.")
-        sys.exit(1)
+    def admin(self):
+        try:
+            if os.name == 'nt':
+                return ctypes.windll.shell32.IsUserAnAdmin() != 0
+        
+        except Exception:
+            return False
     
-    except Exception as e:
-        print(f"[-] Error has been occured:{e}")
+    def read_logs(self):
+        while self.is_running and self.process:
+            line = self.process.stdout.readline()
+            if line:
+                    clean_line = line.strip()
+                    print(f"[DPI-Engine] {clean_line}")
+
+            if self.process.poll() is not None:
+                break
+
+    def start(self, arguments):
+        if not self.is_admin():
+            print("[-] Error: DPI must run as an administor.")
+            sys.exit(1)
     
-def stop(self):
-    if self.process and self.process.poll() is None:
-        print("\n [*] Stopping Service...")
-        self.process.terminate()
-        self.process.wait()
-        print("[+] Service has been shut.")
+        print(f"[*] DPI Bypass tool is starting: {self.executable_path}")
+
+        try:
+            self.process = subprocess.Popen(
+                [self.executable_path] + arguments,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                text=True,
+                bufsize=1
+            )
+            self.is_running=True
+
+            log_thread = threading.Thread(target=self.read_logs, daemon=True)
+            log_thread.start()
+
+            print("[+] Service has been started successfully. Traffic is now hidden.")
+
+        except FileNotFoundError:
+            print("[-] Error: No  file found to execute. Check the path.")
+            sys.exit(1)
+    
+    def stop(self):
+        if self.process and self.process.poll() is None:
+            print("\n [*] Stopping Service...")
+            self.is_running = False
+            self.process.terminate()
+            self.process.wait()
+            print("[+] Service has been shut.")
 
 if __name__ == "__main__":
     TOOL_PATH = "./goodbyedpi.exe"
-
     PARAMS = ["-5"]
 
-    manager = TOOL_PATH
+    manager = DPIBypassManager(TOOL_PATH)
 
     try:
         manager.start(PARAMS)
